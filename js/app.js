96cafaad6fa228a0d4a05a32318d5836e22f92b5
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-12-18 23:48:03 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-nextt",
-    prevEl: ".swiper-button-prevv",
+    nextEl: '.swiper-button-nextt',
+    prevEl: '.swiper-button-prevv',
   },
 });
 
-const bars = document.querySelector(".bars");
-const navlink = document.querySelector(".header_links");
+const bars = document.querySelector('.bars');
+const navlink = document.querySelector('.header_links');
 
-bars.addEventListener("click", function () {
-  navlink.classList.toggle("nav_active");
+bars.addEventListener('click', function () {
+  navlink.classList.toggle('nav_active');
 });
+
+function toggleDropdown() {
+  let options = document.getElementById('options');
+  options.style.display =
+    options.style.display === 'block' ? 'none' : 'block';
+}
+
+function selectOption(option) {
+  let selectBox = document.querySelector('.select-box span');
+  selectBox.textContent = option;
+  toggleDropdown();
+}
+
+
+function toggleDropdownKuziv() {
+  let options = document.getElementById('optionsKuziv');
+  options.style.display =
+    options.style.display === 'block' ? 'none' : 'block';
+}
+
+function selectOptionKuziv(option) {
+  let selectBox = document.querySelector('.select-box-kuziv span');
+  selectBox.textContent = option;
+  toggleDropdownKuziv();
+}
