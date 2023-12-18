58a52f9aac5806df5c5ff10b78e422add7ec818f
shomurodovX <cometa11we@gmail.com>
2023-12-18 08:22:17 +0500

    asdasd

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-nextt",
+    prevEl: ".swiper-button-prevv",
+  },
+});
